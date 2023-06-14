e4dd76a7999e7ededfb36b580497562a9a7f68e5
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-06-14 16:44:58 -0400

    first commit

---
+++ operaciones.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+
+const registrar = (nombre, edad, tipo, color, enfermedad)=>{
+
+    let citas = []
+
+    try {
+        const leerCitas = fs.readFileSync('citas.JSON', 'utf-8')
+        citas = JSON.parse(leerCitas)       
+    } catch (error) {
+        console.log(error)
+    }
+
+    let nuevaCita = 
+        {
+        "Nombre del Animal": nombre,
+        "Edad": edad,
+        "Tipo de animal": tipo,
+        "Color del animal": color,
+        "enfermedad": enfermedad, 
+        }       
+    
+
+    citas.push(nuevaCita)
+    fs.writeFileSync('citas.json', JSON.stringify(citas));
+}
+
+const leer  = ()=>{
+    const leerCitas = fs.readFileSync('citas.JSON', 'utf-8')
+
+    JSON.parse(leerCitas).forEach(paciente => {
+        console.log(paciente) // el desafio no deja claro en formato se debe presentar esta info en consola, que atento al feedback.
+    });
+}
+
+module.exports ={
+    registrar,
+    leer
+}
